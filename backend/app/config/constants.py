7532532c6067f405171eebcad132c096cb555ca9
0rphion <belisairus01@gmail.com>
2025-08-26 04:20:56 +0300

    Create constants.py
    Summary: Common constants defined.
    
    - QLK_SUPPORTED_DEX: List of supported exchanges.
    
    - DEFAULT_AMOUNT_USD: Default transaction size.
    
    - DEX_LABELS: Human-readable equivalents of DEX names.

---
+++ backend/app/config/constants.py
@@ -0,0 +1,36 @@
+QLK_SUPPORTED_DEX = {
+    "uniswap_v2",
+    "uniswap_v3",
+    "sushiswap",
+    "curve",
+    "balancer",
+}
+
+DEX_CHAINS = {
+    "uniswap_v2": "ethereum",
+    "uniswap_v3": "ethereum",
+    "sushiswap":  "ethereum",
+    "curve":      "ethereum",
+    "balancer":   "ethereum",
+}
+
+GAS_COST_USD_DEFAULT = {
+    "ethereum": 20.0,
+    "arbitrum": 1.0,
+    "polygon":  0.10,
+    "bsc":      0.05,
+}
+
+GAS_COSTS_USD = {
+    "ethereum": 20.0,
+    "arbitrum": 1.0,
+    "polygon":  0.10,
+    "bsc":      0.05,
+}
+
+BRIDGE_COSTS_USD = {
+    "ethereum": 5.0,
+    "arbitrum": 2.0,
+    "polygon":  1.0,
+    "bsc":      1.0,
+}
